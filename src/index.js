421ad7055cc45ef4321526284dc50857e7c46dc7
Gabriel Théron <gabriel.theron@spendesk.com>
2020-09-11 17:56:23 +0200

    Clean up unneeded configuration code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const { parsers: typescriptParsers } = _interopDefault(
   require("prettier/parser-typescript")
 );
 const sortImports = _interopDefault(require("import-sort"));
-const path = _interopDefault(require("path"));
+const parser = _interopDefault(require("./parser"));
+const style = _interopDefault(require("./style"));
 
 function throwIf(condition, message) {
   if (condition) {
@@ -16,58 +17,30 @@ function throwIf(condition, message) {
   }
 }
 
-function getAndCheckConfig(extension, fileDirectory) {
-  const resolvedConfig = {
-    config: { parser: "typescript", style: "module" },
-    parser: _interopDefault(require("./parser")),
-    style: _interopDefault(require("./style")),
-  };
-
-  throwIf(!resolvedConfig, `No configuration found for file type ${extension}`);
-
-  const rawParser = resolvedConfig.config.parser;
-  const rawStyle = resolvedConfig.config.style;
-
-  throwIf(!rawParser, `No parser defined for file type ${extension}`);
-  throwIf(!rawStyle, `No style defined for file type ${extension}`);
-
-  const { parser, style } = resolvedConfig;
-
-  throwIf(!parser, `Parser "${rawParser}" could not be resolved`);
-  throwIf(
-    !style || style === rawStyle,
-    `Style "${rawStyle}" could not be resolved`
-  );
-
-  return resolvedConfig;
-}
-
 function organizeImports(unsortedCode, extension) {
   // this throw exceptions up to prettier
-  const config = getAndCheckConfig(
-    extension,
-    path.resolve(__dirname, "..", "..")
+  throwIf(
+    !["ts", "tsx"].includes(extension),
+    `No configuration found for file type ${extension}`
   );
-  const { parser, style, config: rawConfig } = config;
+
+  throwIf(!style, `No style loaded`);
+  throwIf(!parser, `No parser loaded`);
 
   const sortResult = sortImports(
     unsortedCode,
     parser,
     style,
-    `dummy${extension}`,
-    rawConfig.options
+    `dummy${extension}`
   );
 
   return sortResult.code;
 }
 
-console.log("exporting parser");
-
 const parsers = {
   typescript: {
     ...typescriptParsers.typescript,
     preprocess(text) {
-      console.log("gonna preprocess");
       return organizeImports(text, ".ts");
     },
   },
